f1db3b59197e04f70307fcbf594ec7c15e20714f
Banggg <133222169+BangCP@users.noreply.github.com>
2024-04-16 15:05:31 +0700

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,72 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
-  const [User, setUser] = useState('')
-  const [password, setPassword] = useState('')
-  const [UserError, setUserError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-
-  const navigate = useNavigate()
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [userError, setUserError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
 
   const onButtonClick = () => {
-    setUserError('')
-    setPasswordError('')
+    setUserError('');
+    setPasswordError('');
 
-    if ('' === User) {
-        setUserError('Please enter your User');
-        return;
+    if (user === '') {
+      setUserError('Please enter your User');
+      return;
     }
 
-    if ('' === password) {
-        setPasswordError('Please enter a password');
-        return;
+    if (password === '') {
+      setPasswordError('Please enter a password');
+      return;
     }
 
-    if (User !== 'admin' || password !== 'admin') {
-        setUserError('User or password wrong');
-        setPasswordError('User or password wrong');
-        return;
+    if (user !== 'admin' || password !== 'admin') {
+      setPasswordError('User or password wrong');
+      return;
     }
 
     navigate('/quan-li-doi-xe');
-  }
+  };
 
   return (
-    <div className={'mainContainer'}>
-      <div className={'titleContainer'}>
-        <div>Login</div>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={User}
-          placeholder="Enter your User here"
-          onChange={(ev) => setUser(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{UserError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={password}
-          type="password"
-          placeholder="Enter your password here"
-          onChange={(ev) => setPassword(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{passwordError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+    <div className="mainContainer">
+      <div className="loginBox">
+        <div className="titleContainer">
+          <div className="title">Login 🔐</div>
+        </div>
+        <div className="inputContainer">
+          <input
+            value={user}
+            placeholder="Enter your User here"
+            onChange={(ev) => setUser(ev.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{userError}</label>
+        </div>
+        <div className="inputContainer">
+          <input
+            value={password}
+            type="password"
+            placeholder="Enter your password here"
+            onChange={(ev) => setPassword(ev.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{passwordError}</label>
+        </div>
+        <div className="inputContainer">
+          <input
+            className="inputButton"
+            type="button"
+            onClick={onButtonClick}
+            value="Log in"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
