99388534926cf82649e1290aa6de3596da553ed9
Banggg <133222169+BangCP@users.noreply.github.com>
2024-04-17 22:25:34 +0700

    Add files via upload

--- src/components/DriversManagement.js
+++ src/components/DriversManagement.js
@@ -25,114 +25,94 @@ function DriversManagement() {
         fetchData();
         }, []);
     
-    //FUNCTION
-    const toggleShowInfo = (driverId) => {
-      setSelectedDriverId(driverId === selectedDriverId ? null : driverId);
-    };
+        const toggleShowInfo = (driverId) => {
+            setSelectedDriverId(driverId === selectedDriverId ? null : driverId);
+          };
+        
+          const handleShowAllDrivers = () => {
+            setShowAllDrivers(!showAllDrivers);
+          };
+        
   
-    const handleShowAllDrivers = () => {
-      setShowAllDrivers(!showAllDrivers);
-    };
-  
-    const showAllBtnStyle = {
-      backgroundColor: showAllDrivers ? '#8ED7B2' : '#BE966F',
-      color: showAllDrivers ? 'black' : 'black',
-      border: '1px solid #ccc',
-      borderRadius: '4px',
-      maxWidth: '90%',
-      fontSize: '10px',
-      cursor: 'pointer',
-      transition: 'background-color 0.3s ease',
-      padding: '5px',
-    };
-
-
-    
-    return (
-        <div>
-          <h2 className='DriverManagement'>Driver Management</h2>
-          <div>
-            <h3 className="dl">Drivers List</h3>
-            <Button className='showAllBtn'
-                onClick={handleShowAllDrivers}
-                style={showAllBtnStyle}
-            >
-                {showAllDrivers ? "Hide all driver's information" : "Show all driver's information"}
-            </Button>
-            {drivers.map((driver) => (
-                <button
-                className="DriverUnit"
-                key={driver.id}
-                style={
-                    driver.status === 'Ready'
-                    ? { backgroundColor: 'lightgreen' }
-                    : driver.status === 'Not ready'
-                    ? { backgroundColor: 'lightcoral' }
-                    : {}
-                }
-                onClick={() => toggleShowInfo(driver.id)}
+          const showAllBtnStyle = {
+            backgroundColor: showAllDrivers ? '#8ED7B2' : '#BE966F',
+            color: showAllDrivers ? 'black' : 'black',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            maxWidth: '90%',
+            fontSize: '1px',
+            cursor: 'pointer',
+            transition: 'background-color 0.3s ease',
+            padding: '5px',
+          };
+        
+          return (
+            <div>
+              <h2 className='DriverManagement'>Driver Management</h2>
+              <div>
+                <h3 className="dl">Drivers List</h3>
+                <Button
+                  className='showAllBtn'
+                  onClick={handleShowAllDrivers}
+                  style={showAllBtnStyle}
                 >
-                <strong className="DriverName">{driver.name}</strong>
-                {(selectedDriverId === driver.id || showAllDrivers) && (
+                  {showAllDrivers ? "Hide all driver's information" : "Show all driver's information"}
+                </Button>
+                {drivers.map((driver) => (
+                  <button
+                    className={`DriverUnit ${selectedDriverId === driver.id || showAllDrivers ? 'expanded' : ''}`}
+                    key={driver.id}
+                    style={
+                      driver.status === 'Ready'
+                        ? { backgroundColor: 'lightgreen' }
+                        : driver.status === 'Not ready'
+                        ? { backgroundColor: 'lightcoral' }
+                        : {}
+                    }
+                    onClick={() => toggleShowInfo(driver.id)}
+                  >
+                    <strong className="DriverName">{driver.name}</strong>
                     <ul className="DriverDetails">
-
-                    <li>Age: 
-                        {driver.age ? driver.age : 'unknown'} 
-                    </li>
-
-                    <li>License Type: 
-                        {driver.licenseType ? driver.licenseType : 'unknown'}
-                    </li>
-
-                    <li>Phone:
-                        {driver.phone ? driver.phone : 'unknown'}
-                    </li>
-
-                    <li>Address: 
-                        {driver.address ? driver.address : 'unknown'}
-                    </li>
-
-                    <li>Driving History: 
-                        {driver.drivingHistory ? driver.drivingHistory : 'unknown'}
-                    </li>
-
-                    <li>Status: {driver.status}</li>
-
-                    {driver.image ? (
+                      <li>Age: {driver.age ? driver.age : 'unknown'}</li>
+                      <li>License Type: {driver.licenseType ? driver.licenseType : 'unknown'}</li>
+                      <li>Phone: {driver.phone ? driver.phone : 'unknown'}</li>
+                      <li>Address: {driver.address ? driver.address : 'unknown'}</li>
+                      <li>Driving History: {driver.drivingHistory ? driver.drivingHistory : 'unknown'}</li>
+                      <li>Status: {driver.status}</li>
+                      {driver.image ? (
                         <img
-                        src={driver.image}
-                        alt="Driver Image"
-                        style={{
+                          src={driver.image}
+                          alt="Driver Image"
+                          style={{
                             width: '150px',
                             maxWidth: '100%',
                             border: '2px solid black',
                             borderRadius: '5px',
                             display: 'inline-block',
                             marginRight: 'auto',
-                        }}
+                          }}
                         />
-                    ) : (
+                      ) : (
                         <img
-                        src="https://www.shutterstock.com/shutterstock/photos/535853263/display_1500/stock-vector-profile-photo-vector-placeholder-pic-male-person-default-profile-gray-photo-picture-avatar-535853263.jpg"
-                        alt="Broken Image"
-                        style={{
+                          src="https://www.shutterstock.com/shutterstock/photos/535853263/display_1500/stock-vector-profile-photo-vector-placeholder-pic-male-person-default-profile-gray-photo-picture-avatar-535853263.jpg"
+                          alt="Broken Image"
+                          style={{
                             width: '150px',
                             maxWidth: '100%',
                             border: '2px solid black',
                             borderRadius: '5px',
                             display: 'inline-block',
                             marginRight: 'auto',
-                        }}
+                          }}
                         />
-                    )}
+                      )}
                     </ul>
-                )}
-                </button>
-            ))}
+                  </button>
+                ))}
+              </div>
+              <div className="divider"></div>
             </div>
-            <div className="divider"></div>
-        </div>
-    );
-}
-  
-  export default DriversManagement;
+          );
+        }
+        
+        export default DriversManagement;
--- src/components/VehiclesManagement.js
+++ src/components/VehiclesManagement.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import { collection, getDocs } from "firebase/firestore"; 
-import { firebase } from '../firebase'
+import { collection, getDocs } from "firebase/firestore";
+import { firebase } from '../firebase';
 
 function VehiclesManagement({ VehicleIdPass }) {
   const [vehicles, setData] = useState([]);
@@ -36,7 +36,7 @@ function VehiclesManagement({ VehicleIdPass }) {
     border: '1px solid #ccc',
     borderRadius: '4px',
     maxWidth: '90%',
-    fontSize: '10px',
+    fontSize: '1px',
     cursor: 'pointer',
     transition: 'background-color 0.3s ease',
     padding: '5px',
@@ -44,98 +44,78 @@ function VehiclesManagement({ VehicleIdPass }) {
 
   const toggleShowInfo = (vehicleId) => {
     setSelectedVehicle(vehicleId === selectedVehicle ? null : vehicleId);
-    VehicleIdPass(selectedVehicle !== vehicleId ? vehicleId : null)
+    VehicleIdPass(selectedVehicle !== vehicleId ? vehicleId : null);
   };
 
-  
-  
   return (
     <div>
       <h2 className='VehicleManagement'>Vehicle Management</h2>
       <div>
         <h3 className="vl">Vehicles List</h3>
         <Button
-            onClick={handleShowAllVehicles}
-            style={showAllBtnStyle}
+          onClick={handleShowAllVehicles}
+          style={showAllBtnStyle}
         >
-            {showAllVehicles ? "Hide all vehicle's information" : "Show all vehicle's information"}
+          {showAllVehicles ? "Hide all vehicle's information" : "Show all vehicle's information"}
         </Button>
         {vehicles.map((vehicle) => (
-        <button
-        className="VehicleUnit"
-        key={vehicle.id}
-        style={
-            vehicle.status === 'Working'
-            ? { backgroundColor: 'lightcoral' }
-            : vehicle.status === 'Maintenance'
-            ? { backgroundColor: '#737373' }
-            : vehicle.status === 'Inactive'
-            ? { backgroundColor: 'lightgreen' }
-            : {}
-        }
-        onClick={() => toggleShowInfo(vehicle.id)}
-        >
-        <strong className="VehicleName">{vehicle.name}</strong>
-        {(selectedVehicle === vehicle.id || showAllVehicles) && (
-          <ul className="VehicleDetails">
-
-          <li>Size: 
-              {vehicle.size ? vehicle.size : 'unknown'}
-          </li>
-
-          <li>Payload: 
-              {vehicle.payload ? vehicle.payload : 'unknown'}
-          </li>
-
-          <li>Fuel Type: 
-              {vehicle.fuel ? vehicle.fuel : 'unknown'}
-          </li>
-
-          <li>Status: 
-              {vehicle.status ? vehicle.status : 'unknown'}
-          </li>
-
-          <li>License Plate: 
-              {vehicle.licensePlate ? vehicle.licensePlate : 'unknown'}
-          </li>
-
-          {vehicle.image ? (
-              <img
-              src={vehicle.image}
-              alt={vehicle.name}
-              className="VehicleImage"
-              style={{
-                  width: '150px',
-                  maxWidth: '100%',
-                  border: '2px solid black',
-                  borderRadius: '5px',
-                  display: 'inline-block',
-                  marginRight: 'auto',
-              }}
-              />
-          ) : (
-              <img
+          <button
+            className={`VehicleUnit ${selectedVehicle === vehicle.id || showAllVehicles ? 'expanded' : ''}`}
+            key={vehicle.id}
+            style={
+              vehicle.status === 'Working'
+                ? { backgroundColor: 'lightcoral' }
+                : vehicle.status === 'Maintenance'
+                ? { backgroundColor: '#737373' }
+                : vehicle.status === 'Inactive'
+                ? { backgroundColor: 'lightgreen' }
+                : {}
+            }
+            onClick={() => toggleShowInfo(vehicle.id)}
+          >
+            <strong className="VehicleName">{vehicle.name}</strong>
+            <ul className="VehicleDetails">
+              <li>Size: {vehicle.size ? vehicle.size : 'unknown'}</li>
+              <li>Payload: {vehicle.payload ? vehicle.payload : 'unknown'}</li>
+              <li>Fuel Type: {vehicle.fuel ? vehicle.fuel : 'unknown'}</li>
+              <li>Status: {vehicle.status ? vehicle.status : 'unknown'}</li>
+              <li>License Plate: {vehicle.licensePlate ? vehicle.licensePlate : 'unknown'}</li>
+              {vehicle.image ? (
+                <img
+                  src={vehicle.image}
+                  alt={vehicle.name}
+                  className="VehicleImage"
+                  style={{
+                    width: '150px',
+                    maxWidth: '100%',
+                    border: '2px solid black',
+                    borderRadius: '5px',
+                    display: 'inline-block',
+                    marginRight: 'auto',
+                  }}
+                />
+              ) : (
+                <img
                   src="https://t3.ftcdn.net/jpg/02/33/69/46/360_F_233694647_8fEIOuq6QVxjFHTOhLrN3xBLYAnP8WA7.jpg"
                   alt="BrkImage"
                   className="VehicleImage"
                   style={{
-                  width: '150px',
-                  maxWidth: '100%',
-                  border: '2px solid black',
-                  borderRadius: '5px',
-                  display: 'inline-block',
-                  marginRight: 'auto',
+                    width: '150px',
+                    maxWidth: '100%',
+                    border: '2px solid black',
+                    borderRadius: '5px',
+                    display: 'inline-block',
+                    marginRight: 'auto',
                   }}
-              />
-          )}
-          </ul>
-        )}
-        </button>
+                />
+              )}
+            </ul>
+          </button>
         ))}
       </div>
       <div className="divider"></div>
     </div>
   );
 }
-  
-  export default VehiclesManagement;
+
+export default VehiclesManagement;
